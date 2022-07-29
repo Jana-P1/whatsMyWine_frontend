1a756430891aed83a450f2fccc905a394822027e
Jana Pascual <jacpascual@gmail.com>
2022-07-29 16:50:26 -0400

    Add google script to index.html file in Public folder

---
+++ src/components/auth/login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useGoogleLogin } from "@react-oauth/google";
+
+const Login = () => {
+  const login = useGoogleLogin({
+    onSuccess:
+  })
+  return (  );
+}
+ 
+export default Login;
