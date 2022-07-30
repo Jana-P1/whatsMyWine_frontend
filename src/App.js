e161990bc9ca2d1e38c1650be12a60e52d5f8815
Jana Pascual <jacpascual@gmail.com>
2022-07-30 08:36:57 -0400

    Add google sign in button and functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/auth/login'
 
 function App() {
+  
   return (
     <div className="App">
       <Login />
--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,11 +1,29 @@
+
 import React from "react";
-import { useGoogleLogin } from "@react-oauth/google";
+import { useEffect } from "react";
 
 const Login = () => {
-  const login = useGoogleLogin({
-    onSuccess:
-  })
-  return (  );
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID Token" + response.credential)
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '275135025973-l3f8cuvqu6a8n4680gcfbtdb62nbo80e.apps.googleusercontent.com',
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      { theme: 'outline', size: 'large' }
+    );
+  }, [])
+  
+  return ( 
+    <div>
+      <div id="signInDiv"></div>
+    </div>
+   );
 }
  
 export default Login;
