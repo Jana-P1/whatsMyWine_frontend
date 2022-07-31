334cd619f5a3c782f8a78a8dc1c7dc837eab243f
Jana Pascual <jacpascual@gmail.com>
2022-07-31 12:08:59 -0400

    Add url to frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/auth/login'
+import jwtDecode from 'jwt-decode';
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
-  
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID Token" + response.credential);
+    let userObject = jwtDecode(response.credential);
+    console.log(userObject)
+  }
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '275135025973-l3f8cuvqu6a8n4680gcfbtdb62nbo80e.apps.googleusercontent.com',
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      { 
+        theme: 'outline', 
+        size: 'large',
+        shape: 'circle',
+        text: 'sigin_with',
+        logo_alignment: 'center' 
+      }
+    ); google.accounts.id.prompt()
+  }, [])
   return (
     <div className="App">
-      <Login />
+      <NavBar use={user} />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const NavBar = ({ user, handleLogout }) => {
+  
+  return ( 
+
+   );
+}
+ 
+export default NavBar
