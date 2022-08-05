0abdd4791d6676d1bd07ba26a1ce167424c0dcea
Jana Pascual <jacpascual@gmail.com>
2022-08-05 13:32:52 -0400

    Build googleLogin functionality. Function not yet complete

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,6 +6,7 @@ const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/auth`
 export function googleSuccessSignIn(object) {
   if(object) {
     console.log(object)
+    googleLogin(object)
   } else {
     console.log("This didn't work")
   }
@@ -14,6 +15,12 @@ export function googleSuccessSignIn(object) {
   }
   
 
-// async function googleLogin(credentials) {
-   
-// }
+async function googleLogin(credentials) {
+  console.log("sanity check for googleLogin()")
+  try {
+    const res = await fetch()
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
